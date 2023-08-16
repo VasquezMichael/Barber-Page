708bf0e40709226961ee7c12f03cdaec1187cddc
VasquezMichael <vasquez.michael.steven@gmail.com>
2023-08-15 21:00:41 -0300

    Agregamos TeamImg a ourTeam

--- src/components/OurTeam/index.jsx
+++ src/components/OurTeam/index.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { SectionTeam, ContainerTeam } from "./style";
 // import { TeamInfo } from "./TeamInfo";
-// import TeamImage from "./TeamImg";
+import TeamImage from "./TeamImg";
 const OurTeam = () => {
   return (
     <SectionTeam>
       <ContainerTeam>
-        {/* <TeamInfo />
-        <TeamImage /> */}
+        <TeamImage />
         <p>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem,
           doloribus fuga, quibusdam error, tempora hic doloremque voluptatibus
